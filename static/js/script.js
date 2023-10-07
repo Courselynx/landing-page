5c40a82a5f036cf53b88526e16eb5eb217ad6c87
Christian Graham <christian.graham@ufl.edu>
2023-10-07 08:57:27 -0400

    phone styles 9

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,18 @@
 // Get the .box element
 const box = document.querySelector('.box');
+const dbs = document.querySelector('.dbs');
+const photo = document.querySelector('.phoneputer')
 
 // Listen for the scroll event on the window
 window.addEventListener('scroll', () => {
-    console.log("window", window.innerHeight/2)
     console.log("y", window.scrollY)
-    if (window.scrollY >= window.innerHeight/2) {
-        console.log("made it")
+    if (window.scrollY >= window.innerHeight/1.6) {
         // Add the animate.css class to trigger the animation
         box.classList.add('animate__animated', 'animate__fadeInUp');
+    }
+    if (window.scrollY >= window.innerHeight/0.8){
+        console.log("made it")
+        dbs.classList.add('animate__animated', 'animate__fadeInLeft');
+        photo.classList.add('animate__animated', 'animate__fadeInRight');
     }
 });
