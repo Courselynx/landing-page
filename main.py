da365c7fa96b1b9ee612e1069d67eb49a7cf17cd
Christian Graham <christian.graham@ufl.edu>
2023-10-13 19:15:25 -0400

    added email to csv file

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-from flask import Flask, render_template, url_for, jsonify, request
+from flask import Flask, render_template, url_for, jsonify, request, redirect, flash
 from flask_cors import CORS
 import csv
+import re
 
 app = Flask(__name__)
 CORS(app)
+
+# This is necessary for flashing messages
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/save-email', methods=['POST'])
-def save_email():
-    email = request.json.get('email')
+EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    email = request.form.get('email')
     
-    if not email:
-        return jsonify(success=False, message="No email provided"), 400
+    if not EMAIL_REGEX.match(email):
+        flash('Invalid email format. Please enter a valid email address.', 'error')
+        return redirect(url_for('index'))
 
-    try:
-        with open('output/emails.csv', 'a', newline='') as csvfile:
-            email_writer = csv.writer(csvfile)
-            email_writer.writerow([email])
-        return jsonify(success=True, message="Email saved successfully")
-    except Exception as e:
-        print(f"Error writing to CSV: {e}")
-        return jsonify(success=False, message="Error saving email"), 500
-
+    with open('output/subscribers.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([email])
+    
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
